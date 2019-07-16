0f7a2cc8ed49fdd3c48a7e38a0b18b91e4066d09
yonglang <yonglang>
2019-07-16 19:00:26 +0800

    dao_oal.h

--- dao/oal/dao_oal.h
+++ dao/oal/dao_oal.h
@@ -2,8 +2,9 @@
 #define _DAO_OAL_H
 
 void dao_read_create_config_file(char *file, void *root_config);
-void *dao_load_default_config_file_singular(char *file,char *parent_element,char *element,char *string,int val);
-
+void *dao_load_default_config_file_singular(char *file,const char *parent_element,const char *element,const char *string,int val);
+const char *dao_load_config_file_singular(char *file,const char *parent_element,const char *element);
+const char *dao_load_config_file_array(char *file,const char *array,const char *name,const char *value,const char *element);
 
 #endif
 

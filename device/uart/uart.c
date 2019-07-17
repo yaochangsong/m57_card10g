f709d4605419407776d4f4bbe39f0b5fb83bc0a3
chenda <2643924837@qq.com>
2019-07-17 17:20:14 +0800

    修改uart文件

--- device/uart/uart.c
+++ device/uart/uart.c
@@ -1,31 +1,28 @@
 #include<config.h>
-
-#define RET_TRUE 0
-#define RET_FALSE 1
 unsigned long  speed_arr[] = {B115200,B57600,B38400, B19200, B9600, B4800, B2400, B1200, B300,
           B38400, B19200, B9600, B4800, B2400, B1200, B300, };
-unsigned long name_arr[] = {115200,57600,38400,  19200,  9600,  4800,  2400,  1200,  300, 38400,  
+unsigned long name_arr[] = {115200,57600,38400,  19200,  9600,  4800,  2400,  1200,  300, 38400,
           19200,  9600, 4800, 2400, 1200,  300, };
 
 static void set_speed(int fd, unsigned long speed)
 {
- int   i; 
- int   status; 
- struct termios   Opt;
- tcgetattr(fd, &Opt); 
-  for ( i= 0;  i < sizeof(speed_arr) / sizeof(int);  i++) { 
-   if  (speed == name_arr[i]) {     
-     tcflush(fd, TCIOFLUSH);     
-     cfsetispeed(&Opt, speed_arr[i]);  /*对于波特率的设置函数*/
-     cfsetospeed(&Opt, speed_arr[i]);   
-     status = tcsetattr(fd, TCSANOW, &Opt);  
-     if  (status != 0) {        
-      perror("tcsetattr fd");  
-      return;     
-     }    
-     tcflush(fd,TCIOFLUSH);   
-  }  
- }
+    int   i;
+    int   status;
+    struct termios   Opt;
+    tcgetattr(fd, &Opt); 
+    for ( i= 0;  i < sizeof(speed_arr) / sizeof(int);  i++) {
+    if  (speed == name_arr[i]) {
+        tcflush(fd, TCIOFLUSH);
+        cfsetispeed(&Opt, speed_arr[i]);  /*对于波特率的设置函数*/
+        cfsetospeed(&Opt, speed_arr[i]);
+        status = tcsetattr(fd, TCSANOW, &Opt);
+        if  (status != 0) {
+            perror("tcsetattr fd");
+            return;
+            }
+tcflush(fd,TCIOFLUSH);
+        }
+    }
 }
 
 /**
@@ -36,112 +33,112 @@ static void set_speed(int fd, unsigned long speed)
 *@param  parity  类型  int  效验类型 取值为N,E,O,,S
 */
 static int set_Parity(int fd,int databits,int stopbits,int parity)
-{ 
- struct termios options; 
- if  ( tcgetattr( fd,&options)  !=  0) { 
-  perror("SetupSerial 1");     
-  return(RET_FALSE);  
- }
-    options.c_lflag &= ~(ICANON|ECHO|ECHOE|ECHOK|ECHONL|NOFLSH|ISIG);
-    options.c_oflag = 0;
-	options.c_cflag &= ~CSIZE; 
-    options.c_iflag &= ~(BRKINT | ICRNL | ISTRIP | IXON);
-    options.c_iflag &= ~(IGNCR);
- switch (databits) /*设置数据位数*/
- {   
-  case 7:		
-   options.c_cflag |= CS7; 
-   break;
-  case 8:     
-   options.c_cflag |= CS8;
-   break;   
-  default:    
-   printf_warn("Unsupported data size\n"); return (RET_FALSE);  
- }
- switch (parity) 
- {   
-  case 'n':
-  case 'N':    
+{
+    struct termios options;
+    if  ( tcgetattr( fd,&options)  !=  0)
+    { 
+    perror("SetupSerial 1");
+    return(false);
+    }
+options.c_lflag &= ~(ICANON|ECHO|ECHOE|ECHOK|ECHONL|NOFLSH|ISIG);
+options.c_oflag = 0;
+options.c_cflag &= ~CSIZE;
+options.c_iflag &= ~(BRKINT | ICRNL | ISTRIP | IXON);
+options.c_iflag &= ~(IGNCR);
+switch (databits) /*设置数据位数*/
+    {
+    case 7:		
+    options.c_cflag |= CS7;
+    break;
+    case 8:
+    options.c_cflag |= CS8;
+    break;
+    default:
+    printf_warn("Unsupported data size\n"); return (false);
+    }
+switch (parity) 
+    {
+    case 'n':
+    case 'N':    
     options.c_cflag &= ~PARENB;   /* Clear parity enable */
-    options.c_iflag &= ~INPCK;     /* Enable parity checking */ 
+    options.c_iflag &= ~INPCK;     /* Enable parity checking */
             options.c_iflag &= ~(ICRNL | IGNCR);
             options.c_lflag &= ~(ICANON);
-   break;  
-  case 'o':   
-  case 'O':     
-   options.c_cflag |= (PARODD | PARENB); /* 设置为奇效验*/  
-   options.c_iflag |= INPCK;             /* Disnable parity checking */ 
-   break;  
-  case 'e':  
-  case 'E':   
-   options.c_cflag |= PARENB;     /* Enable parity */    
-   options.c_cflag &= ~PARODD;   /* 转换为偶效验*/     
-   options.c_iflag |= INPCK;       /* Disnable parity checking */
-   break;
-  case 'S': 
-  case 's':  /*as no parity*/   
-   options.c_cflag &= ~PARENB;
-   options.c_cflag &= ~CSTOPB;break;  
-  default:   
-   printf_warn("Unsupported parity\n");    
-   return (RET_FALSE);  
- }  
- /* 设置停止位*/  
- switch (stopbits)
- {   
-  case 1:    
-   options.c_cflag &= ~CSTOPB;  
-   break;  
-   case 2:    
-   options.c_cflag |= CSTOPB;  
-   break;
-  default:    
-   printf_warn("Unsupported stop bits\n");  
-   return (RET_FALSE); 
- } 
- /* Set input parity option */ 
- if (parity != 'n')   
-  options.c_iflag |= INPCK; 
- options.c_cc[VTIME] = 100; /* 设置超时15 seconds*/   
- options.c_cc[VMIN] = 0; /* Update the options and do it NOW */
- tcflush(fd,TCIFLUSH);
- if (tcsetattr(fd,TCSANOW,&options) != 0)   
- { 
-  perror("SetupSerial 3");   
-  return (RET_FALSE);  
- }
-
- return (RET_TRUE);  
+    break;  
+    case 'o':   
+    case 'O':     
+    options.c_cflag |= (PARODD | PARENB); /* 设置为奇效验*/
+    options.c_iflag |= INPCK;             /* Disnable parity checking */
+    break;  
+    case 'e':  
+    case 'E':   
+    options.c_cflag |= PARENB;     /* Enable parity */
+    options.c_cflag &= ~PARODD;   /* 转换为偶效验*/
+    options.c_iflag |= INPCK;       /* Disnable parity checking */
+    break;
+    case 'S':
+    case 's':  /*as no parity*/
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;break;
+    default:
+    printf_warn("Unsupported parity\n");
+    return (false);
+    }
+/* 设置停止位*/
+switch (stopbits)
+    {
+    case 1:
+    options.c_cflag &= ~CSTOPB;
+    break;
+    case 2:
+    options.c_cflag |= CSTOPB;
+    break;
+    default:
+    printf_warn("Unsupported stop bits\n");
+    return (false);
+    }
+/* Set input parity option */
+if (parity != 'n')
+options.c_iflag |= INPCK;
+options.c_cc[VTIME] = 100; /* 设置超时15 seconds*/
+options.c_cc[VMIN] = 0; /* Update the options and do it NOW */
+tcflush(fd,TCIFLUSH);
+if (tcsetattr(fd,TCSANOW,&options) != 0)
+    {
+    perror("SetupSerial 3");
+    return (false);
+    }
+return (true);
 }
 
 
 static int openserial(char *name)
 {
- int fd =-1;
- fd = open(name,O_RDWR| O_NOCTTY|O_NDELAY);
- if(-1 == fd){
-  perror("open serial 1 failed\n");
- }else{
-    fcntl(fd, F_SETFL, 0);
- }
+    int fd =-1;
+    fd = open(name,O_RDWR| O_NOCTTY|O_NDELAY);
+    if(-1 == fd){
+        perror("open serial 1 failed\n");
+    }else{
+        fcntl(fd, F_SETFL, 0);
+    }
 
- return fd;
+    return fd;
 }
 
 int uart_init(char *name)
 {
 
- int fd;
- fd = openserial(name);
- if(fd<0)return -1;
- 
- set_speed(fd, 115200);
- if (set_Parity(fd,8,1,'N') == RET_FALSE)  {
-  printf_note("Set Parity Error\n");
-  exit (0);
- }	
- usleep(100);
- return fd;
+    int fd;
+    fd = openserial(name);
+    if(fd<0)return -1;
+
+    set_speed(fd, 115200);
+    if (set_Parity(fd,8,1,'N') == false)  {
+        printf_note("Set Parity Error\n");
+        exit (0);
+    }
+    usleep(100);
+    return fd;
 }
 
 char *uart_write_read(int fd ,char *cmd,unsigned int cmd_len ,int *recv_len){

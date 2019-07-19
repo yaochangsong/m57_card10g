6bc60355e439992c9c5800558998ec9b412494e3
yonglang <550281512@qq.com>
2019-07-19 17:22:20 +0800

    dao_oal.h

--- dao/oal/dao_oal.h
+++ dao/oal/dao_oal.h
@@ -47,8 +47,8 @@ int val,char series);
 
 
 void dao_read_create_config_file(char *file,void *root_config);
-void *write_struc_config_file_single(spectrum_single        spectrum_fre);
-void *write_struc_config_file_array(spectrum         spectrum_fre);
+void *write_struc_config_file_single(spectrum_single           spectrum_fre);
+void *write_struc_config_file_array(spectrum           spectrum_fre);
 const char *read_config_file_single(const char *parent_element,const char *element);
 const char *read_config_file_array(const char *array,const char *name,const char *value,const char *element);
 

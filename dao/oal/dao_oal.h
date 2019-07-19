cc2c2cef8727fec43869ea3a23b20095ec603885
yonglang <550281512@qq.com>
2019-07-19 09:50:28 +0800

    dao_oal.h

--- dao/oal/dao_oal.h
+++ dao/oal/dao_oal.h
@@ -2,6 +2,7 @@
 #define _DAO_OAL_H
 
 
+
 typedef struct {
 
 char  series;
@@ -17,22 +18,32 @@ const char *seed_name;
 const char *seed_value;
 } spectrum;
 
+typedef struct {
+
+    char  series;
+    int   val;
+    char  *file;
+    const char *string;
+    const char *parent_element;
+    const char *element;
+    const char *s_element;
+    const char *seed_element;
+} spectrum_single;
+
+void *write_config_file_single(char *file,const char *parent_element,const char *element,const char *string,int val);
+void *write_config_file_array(char *file,const char *array,
+const char *name,const char *value,const char *element,const char *seed_element,
+const char *seed_array,const char *seed_name,const char *seed_value,
+int val,char series);
+
 
 
 
 void dao_read_create_config_file(char *file,void *root_config);
-
-void *write_config_file_single(char *file,const char *parent_element,const char *element,
-const char *s_element,const char *seed_element,
-const char *string,int val,char series);
-
-
-void *write_config_file_array(spectrum         spectrum_fre);
-
-
-
-const char *read_config_file_single(char *file,const char *parent_element,const char *element);
-const char *read_config_file_array(char *file,const char *array,const char *name,const char *value,const char *element);
+void *write_struc_config_file_single(spectrum_single        spectrum_fre);
+void *write_struc_config_file_array(spectrum         spectrum_fre);
+const char *read_config_file_single(const char *parent_element,const char *element);
+const char *read_config_file_array(const char *array,const char *name,const char *value,const char *element);
 
 
 #endif

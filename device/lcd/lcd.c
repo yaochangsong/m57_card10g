76dde1954034d12961f3adbca3da931971972140
yaochangsong <changsong.yao@163.com>
2019-07-24 17:53:31 +0800

    修改LCD报错问题

--- device/lcd/lcd.c
+++ device/lcd/lcd.c
@@ -8,7 +8,7 @@ struct lcd_code_convert_st convert_code_t[] ={
     {EX_NETWORK_CMD,  EX_NETWORK_PORT, SCREEN_COMMON_DATA1, SCREEN_PORT,                NULL},
     {EX_MID_FREQ_CMD, EX_MUTE_SW,      -1,                  SCREEN_CHANNEL_NOISE_EN,    NULL},
     {EX_MID_FREQ_CMD, EX_DEC_BW,       -1,                  SCREEN_CHANNEL_BW,          NULL},
-    {EX_RF_FREQ_CMD,  EX_RF_MODE_CODE  -1,                  SCREEN_CHANNEL_MODE,        NULL},
+    {EX_RF_FREQ_CMD,  EX_RF_MODE_CODE, -1,                  SCREEN_CHANNEL_MODE,        NULL},
     {EX_MID_FREQ_CMD, EX_DEC_METHOD,   -1,                  SCREEN_CHANNEL_DECODE_TYPE, NULL},
     {EX_RF_FREQ_CMD,  EX_RF_MID_FREQ , -1,                  SCREEN_CHANNEL_FRQ,         NULL},
     {EX_RF_FREQ_CMD,  EX_RF_MGC_GAIN , -1,                  SCREEN_CHANNEL_MGC,         NULL},
@@ -21,23 +21,32 @@ struct lcd_code_convert_st *lcd_code_convert(exec_cmd cmd, uint8_t type, uint8_t
 {
     uint32_t i;
     int found = 0;
-
+    static struct lcd_code_convert_st lcd_code;
     for(i = 0; i<ARRAY_SIZE(convert_code_t); i++){
         if(convert_code_t[i].ex_cmd == cmd && convert_code_t[i].ex_type == type){
-            convert_code_t[i].arg = arg;
+            lcd_code.ex_cmd = convert_code_t[i].ex_cmd;
+            lcd_code.ex_type = convert_code_t[i].ex_type;
+            lcd_code.lcd_type = convert_code_t[i].lcd_type;
             if(convert_code_t[i].lcd_cmd == -1){
-                if(arg != NULL)
-                    convert_code_t[i].lcd_cmd = *arg;
+                if(arg != NULL){
+                    lcd_code.lcd_cmd = *arg;
+                }
+                else{
+                    printf_note("lcd code not set\n");
+                }
+            }else{
+                lcd_code.lcd_cmd = convert_code_t[i].lcd_cmd;
             }
             found = 1;
-            return &convert_code_t[i];
+            return &lcd_code;
         }
     }
     if(!found){
-        printf_note("lcd code not found\n");
+         printf_note("[%d %d]lcd code not found\n", cmd, type);
         return NULL;
     }
 }
+
 
 /******************************************************************************
 * FUNCTION:
@@ -86,7 +95,7 @@ int8_t lcd_printf(uint8_t cmd, uint8_t type, void *data, uint8_t *arg)
         return -1;
     }
 #ifdef LCD_TYPE == DWIN_K600_SERIAL_SCREEN
-    printf_note("lcd_cmd:%d, lcd_type=%d\n", code->lcd_cmd , code->lcd_type);
+    printf_debug("lcd_cmd:%d, lcd_type=%d\n", code->lcd_cmd , code->lcd_type);
     k600_receive_write_data_from_user(code->lcd_cmd , code->lcd_type, data);
 #endif
     return 0;
@@ -102,13 +111,13 @@ int8_t init_lcd(void)
     lcd_printf(EX_NETWORK_CMD, EX_NETWORK_MASK, &poal_config->network.netmask, NULL);
     lcd_printf(EX_NETWORK_CMD, EX_NETWORK_GW,   &poal_config->network.gateway, NULL);
     lcd_printf(EX_NETWORK_CMD, EX_NETWORK_PORT, &poal_config->network.port, NULL);
-    for(i = 0; i<MAX_RADIO_CHANNEL_NUM; i++){
-        lcd_printf(EX_MID_FREQ_CMD, EX_MUTE_SW,   &poal_config->multi_freq_point_param[i].points[0].noise_en, NULL);
-        lcd_printf(EX_MID_FREQ_CMD, EX_DEC_BW,   &poal_config->multi_freq_point_param[i].points[0].d_bandwith, NULL);
-        lcd_printf(EX_MID_FREQ_CMD, EX_DEC_METHOD,   &poal_config->multi_freq_point_param[i].points[0].d_method, NULL);
-        lcd_printf(EX_MID_FREQ_CMD, EX_RF_MODE_CODE,   &poal_config->rf_para[i].rf_mode_code, NULL);
-        lcd_printf(EX_MID_FREQ_CMD, EX_RF_MID_FREQ,   &poal_config->rf_para[i].mid_freq, NULL);
-        lcd_printf(EX_MID_FREQ_CMD, EX_RF_MGC_GAIN,   &poal_config->rf_para[i].mgc_gain_value, NULL);
+    for(i = 1; i<=MAX_RADIO_CHANNEL_NUM; i++){
+        lcd_printf(EX_MID_FREQ_CMD, EX_MUTE_SW,   &poal_config->multi_freq_point_param[i].points[0].noise_en, &i);
+        lcd_printf(EX_MID_FREQ_CMD, EX_DEC_BW,   &poal_config->multi_freq_point_param[i].points[0].d_bandwith, &i);
+        lcd_printf(EX_MID_FREQ_CMD, EX_DEC_METHOD,   &poal_config->multi_freq_point_param[i].points[0].d_method, &i);
+        lcd_printf(EX_RF_FREQ_CMD,  EX_RF_MODE_CODE,   &poal_config->rf_para[i].rf_mode_code, &i);
+        lcd_printf(EX_RF_FREQ_CMD,  EX_RF_MID_FREQ,   &poal_config->rf_para[i].mid_freq, &i);
+        lcd_printf(EX_RF_FREQ_CMD,  EX_RF_MGC_GAIN,   &poal_config->rf_para[i].mgc_gain_value, &i);
     }
     return 0;
 }

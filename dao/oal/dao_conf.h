2979eaba49687ce22b477587516c122eecac9fe4
xiaochong <752702107@qq.com>
2019-07-29 14:43:56 +0800

    更新dao/oal/dao_conf.h

--- dao/oal/dao_conf.h
+++ dao/oal/dao_conf.h
@@ -3,35 +3,41 @@
 
 #include "config.h"
 
+#define MAX_SIG_CHANNLE 128
+
 #define XMLFILENAME DEFAULT_CONFIGFILE
 
 #define MAX_SIG_CHANNLE 8
+
+
+
 //void dao_conf_save_batch(exec_cmd cmd, uint8_t type, s_config *config);
 
+void* dao_conf_parse_batch(uint8_t classcode, uint8_t methodcode, char *data);
 /*1      中频参数*/
 
 /*静噪开关设置*/
-struct mute_switch{
+struct dao_mute_switch{
     uint8_t channel;
-    int16_t subChannel;  //?
+    int16_t subChannel;
     bool muteSwitch;
 }__attribute__ ((packed));
 
 /*静噪门限设置*/
-struct noise_threshold{
+struct dao_noise_threshold{
     uint8_t channel;
     int16_t   subChannel; 
     int8_t muteThreshold;
 }__attribute__ ((packed));
 
 /*解调方式参数*/
-struct demodulation_way{
+struct dao_demodulation_way{
     uint8_t channel;
     uint8_t decMethodId; 
 }__attribute__ ((packed));
 
 /*音频采样率设置参数*/
-struct audio_sampling_rate{
+struct dao_audio_sampling_rate{
     uint8_t channel;
     int16_t subChannel; 
     float audioSampleRate;
@@ -39,41 +45,41 @@ struct audio_sampling_rate{
 
 /*2        射频参数设置*/
 /*射频参数设置*/
-struct RF_parameters{
+struct dao_RF_parameters{
     uint8_t channel;
     uint8_t rfModeCode;
     uint8_t gainControlMethod;
     int8_t mgcGainValue;
-    uint32_t agcControlTime;       
-    int8_t agcMidFreqOutLevel;  
-    uint32_t rfBandwidth;             
-    uint8_t antennaSelect;         
+    uint32_t agcControlTime;
+    int8_t agcMidFreqOutLevel;
+    uint32_t rfBandwidth;
+    uint8_t antennaSelect;
 }__attribute__ ((packed));
 
 
 /*射频天线选择*/
 
-struct RF_antenna_selection{
+struct dao_RF_antenna_selection{
     uint8_t channel;
     int8_t antennaSelect;
 }__attribute__ ((packed));
 
 /*射频输出衰减*/
-struct Rf_output_attenuation{
+struct dao_Rf_output_attenuation{
     uint8_t channel;
     int8_t rfOutputAttenuation;
 }__attribute__ ((packed));
 
 
 /*射频输入衰减*/
-struct Rf_Input_attenuation{
+struct dao_Rf_Input_attenuation{
     uint8_t channel;
     int8_t rfInputAttenuation;
 }__attribute__ ((packed));
 
 
 /*射频带宽设置*/
-struct Rf_bandwidth_setting{
+struct dao_Rf_bandwidth_setting{
     uint8_t channel;
     uint32_t rfInputAttenuation;
 }__attribute__ ((packed));
@@ -81,9 +87,11 @@ struct Rf_bandwidth_setting{
 
 
 
-/*3     数据输出使能控制*/
+/*3     控制参数*/
+/*数据输出使能控制*/
 
-struct data_output_enable_control{
+
+struct dao_data_output_enable_control{
     uint8_t channel;
     int16_t subChannel;
     bool    psdEnable;
@@ -94,125 +102,125 @@ struct data_output_enable_control{
 }__attribute__ ((packed));
 
 /*校准控制*/
-struct calibration_control{
+struct dao_calibration_control{
     uint8_t channel;
     uint8_t ctrlMode;
     bool    enable;
-    char* midFreq;  //String 
+    uint64_t midFreq;  //String 
     uint32_t refPowerLevel;//?
 }__attribute__ ((packed));
 
 /*本远控制*/
-struct Local_remote_control{
+struct dao_Local_remote_control{
     uint8_t ctrlMode;
 }__attribute__ ((packed));
 
 /*通道电源控制*/
 
 
-struct power_control{
+struct dao_power_control{
     uint8_t channel;
     bool    enable;
 }__attribute__ ((packed));
 
-struct Channel_power_control{
-    struct power_control channelNode[MAX_SIG_CHANNLE];//?
+struct dao_channel_power_control{
+    struct dao_power_control channelNode;//?
 }__attribute__ ((packed));
 
 
 
 /*时间控制*/
 
-struct time_control{
+struct dao_time_control{
     char* timeStamp;
     char* timeZone;
 }__attribute__ ((packed));
 
 /*状态参数*/
 
-/*获取设备基本信息*/
+/*4 获取设备基本信息*/
 
 
-struct soft_version{
+struct dao_soft_version{
     char* app;
     char* kernel;
-    char* ubootp;
+    char* uboot;
     char* fpga;
 }__attribute__ ((packed));           //
 
-struct disk_Node{
+struct dao_disk_Node{
     char* totalSpace;
     char* freeSpace;
 }__attribute__ ((packed));
 
-struct disk_Info{
+struct dao_disk_Info{
     uint16_t diskNum; //?
-    struct disk_Node diskNode;
+    struct dao_disk_Node diskNode;
 }__attribute__ ((packed));     
 
-struct clk_Info{
+struct dao_clk_Info{
     uint8_t inout;
     uint8_t  status;
     char* frequency;
 }__attribute__ ((packed)); 
 
-struct ad_Info{
+struct dao_ad_Info{
     uint8_t status;
 }__attribute__ ((packed)); 
 
 
-struct rf_node{
+struct dao_rf_node{
     uint8_t status;
     char*   temprature;      
 }__attribute__ ((packed));    
 
-struct rf_Info{
+struct dao_rf_Info{
     uint8_t rfnum;
-    //struct rf_node  rfnode[MAX_SIG_CHANNLE];
+    struct dao_rf_node  rfnode;//struct rf_node  rfnode[MAX_SIG_CHANNLE];
 }__attribute__ ((packed));  
 
 
-struct fpga_Info{
+struct dao_fpga_Info{
     char* temprature;
 }__attribute__ ((packed));
 
 
 
-struct Equipment_basic_infor{
-    struct soft_version softVersion;
-    struct disk_Info diskInfo;
-    struct clk_Info  clkInfo;
-    struct ad_Info   adInfo;  
-    struct rf_Info   rfInfo;
-    struct fpga_Info  fpgaInfo;
+struct dao_Equipment_basic_infor{
+    struct dao_soft_version softVersion;
+    struct dao_disk_Info diskInfo;
+    struct dao_clk_Info  clkInfo;
+    struct dao_ad_Info   adInfo;  
+    struct dao_rf_Info   rfInfo;
+    struct dao_fpga_Info  fpgaInfo;
     
 }__attribute__ ((packed));
 
 
 /*通道状态查询*/
-struct channel_Node{
+struct dao_channel_Node{
     uint8_t channel;
     uint8_t   channelStatus; 
     uint32_t  channelSignal;
 }__attribute__ ((packed));  
 
-struct Channel_status_check{
+struct dao_Channel_status_check{
     uint8_t channelNum;
-    //struct channel_Node  channelNode[MAX_SIG_CHANNLE];
+    struct dao_channel_Node  channelNode[MAX_SIG_CHANNLE];
 }__attribute__ ((packed)); 
 
-struct Check_power_status{
+struct dao_Check_power_status{
     uint8_t powerStatus;
 }__attribute__ ((packed));
 
 
-struct Query_storage_state{
+struct dao_Query_storage_state{
     char* diskNum;
-    struct disk_Node diskNode[MAX_SIG_CHANNLE];
+    struct dao_disk_Node diskNode;
 }__attribute__ ((packed));  
 
 
-struct software_version_infor{
+struct dao_software_version_infor{
     char* app;
     char* kernel;
     char* uboot;
@@ -221,11 +229,5 @@ struct software_version_infor{
 }__attribute__ ((packed));  
 
 
-
-
-
-
-
 #endif
-
 

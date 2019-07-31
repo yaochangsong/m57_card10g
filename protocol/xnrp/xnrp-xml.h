7cc4d325f7adac9cc6d85162875181aaaaae52bf
xiaochong <752702107@qq.com>
2019-07-31 17:39:02 +0800

    更新protocol/xnrp/xnrp-xml.h

--- protocol/xnrp/xnrp-xml.h
+++ protocol/xnrp/xnrp-xml.h
@@ -1,5 +1,5 @@
 #ifndef _XNRP_XML_H_
 #define _XNRP_XML_H_
-int8_t xnrp_xml_parse_data(x_class_code cmd, uint8_t type, void *data, uint32_t len);
+int8_t xnrp_xml_parse_data(uint8_t classcode, uint8_t methodcode, void *data, uint32_t len);
 
 #endif

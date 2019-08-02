20088fc6839e43e58975ac8970ee2100d048afa8
xiaochong <752702107@qq.com>
2019-08-02 19:39:56 +0800

    add fft.h

---
+++ executor/fft.h
@@ -0,0 +1,55 @@
+
+
+#ifndef __FFT_H
+#define __FFT_H
+#include "config.h"
+
+
+#define  W      200      //wav trans txt 数组大小
+#define  MM_PI        (3.14159265358979323846264338327950288419716939937510)       // 圆周率atan(1)* 4
+//#define  N  256
+static unsigned int N=1024*1024;
+#define  SIGNALNUM        2000//输入信号数
+#define  THRESHOLD         135//128.999176/门限
+
+#define   SMOOTHPOINT         128//滑动平均滤波计算平均值时所取的点数
+#define   TRANNUM      1024      //每次传1K的点
+#define   PORT        8000
+#define  FILTER_A      0.01
+static unsigned int INTERVALNUM=10000;
+static unsigned int SHIELDPOINTS=20000;
+static unsigned int  INTERVALNUMTOW=2;
+
+typedef struct {
+    int cenfrepointnum;
+    int y[SIGNALNUM];
+    int z[SIGNALNUM];
+    int centfeqpoint[SIGNALNUM];      //上报中心频点，中心频点的集合
+   // int maxcentfeqpoint;
+    float Threshold;
+     int Centerpoint;//取出的唯一一个中心频率点
+     float Bottomnoise;       
+     float maxcentfrequency;
+     int interval;	
+     int bandwidth[SIGNALNUM];      //带宽
+     float arvcentfreq[SIGNALNUM];  //中心频点功率值
+}fft_state;
+
+extern fft_state fftstate;
+
+
+
+void initfftspace(void);
+void freethememory(void);
+
+void DetectionIQDataSpectrum(int bd,short *data,int fftsize ,int datalen);
+void xulitest(void);
+
+
+
+
+
+#endif
+
+
+

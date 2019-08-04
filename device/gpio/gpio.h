736500cc9145c9d47879d12976fad23796ffd8d6
yaochangsong <changsong.yao@163.com>
2019-08-04 10:39:37 +0800

    add gpio

---
+++ device/gpio/gpio.h
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define GPIO_BASE_OFFSET 960
+
+int init_gpio(int spidev_index);
+int set_gpio(int spidev_index,char val);

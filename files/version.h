27eb152071d685fb3545010d0b292630a673252b
xiaochong <752702107@qq.com>
2021-01-28 13:37:12 +0800

    消除file.c警告

--- files/version.h
+++ files/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION_TAG  "c227b066e89e457115abd0ad3b8179965aec0eab "
+#define VERSION_TAG  "2d5efbecd2d78c8b5c8902579182fcb13aed0b68 "
 
 #endif   // end of define VERSION_H_

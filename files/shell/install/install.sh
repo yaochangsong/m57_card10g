bb5db171aecd3c256800e553cf3532e63f923aaf
TS02 <yaochangsong@showay.com>
2021-12-24 14:17:59 +0800

    修改shell

--- files/shell/install/install.sh
+++ files/shell/install/install.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-INS_PLATFORM_DIR="/usr/bin/"
-
+INS_PLATFOR_DIR="/usr/bin/"
+INS_SRC_DIR="/tmp/app_update"
 install_platform()
 {
 	echo "install platform!"
-	install -m 0755 app/platform ${INS_PLATFORM_DIR}
+	install -m 0755 ${INS_SRC_DIR}/app/platform ${INS_PLATFOR_DIR}
 	#install -m 0755 app/shell/platform.sh /etc/init.d/
-	install -m 0755 app/compile.info /etc/
-	install -m 0755 app/platform_version /etc/
+	install -m 0755 ${INS_SRC_DIR}/app/compile.info /etc/
+	install -m 0755 ${INS_SRC_DIR}/app/platform_version /etc/
 	
 	#echo "install router table!"
 	#install -m 0755 app/nr_config.json /etc/
-	#install -m 0755 app/sroute ${INS_PLATFORM_DIR}
+	#install -m 0755 app/sroute ${INS_PLATFOR_DIR}
 	
 	#install -m 0755 app/shell/platform.sh /etc/init.d/
 	#echo "install shell!"
 	#install -m 0755 app/shell/*.sh /etc/
 }
 
+restart_platform()
+{
+	/etc/init.d/platform.sh restart
+}
+
 install_platform
+restart_platform

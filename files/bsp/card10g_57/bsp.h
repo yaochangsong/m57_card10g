8ef799eb7b1894e1cdd3e233a810b868ae8ab2aa
TS02 <yaochangsong@showay.com>
2021-12-25 09:56:39 +0800

    恢复为同步加载

--- files/bsp/card10g_57/bsp.h
+++ files/bsp/card10g_57/bsp.h
@@ -2,7 +2,7 @@
 #define _BSP_H
 
 //#define DEBUG_TEST
-#define LOAD_FILE_ASYN 1
+//#define LOAD_FILE_ASYN 1
 #define KEY_TOOL_ENABLE 1
 #define MAX_RADIO_CHANNEL_NUM 1         /* 最大射频通道数 */
 #define MAX_RF_NUM 1                    /* 最大射频数 */

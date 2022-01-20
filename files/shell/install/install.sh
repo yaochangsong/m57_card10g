9800ae206409470c355219b452391242618d976b
yaochangsong <yaochangsong@showay.com>
2022-01-20 10:58:10 +0800

    增加platform log切割定时任务;修改脚本

--- files/shell/install/install.sh
+++ files/shell/install/install.sh
@@ -6,6 +6,9 @@ LOG_DIR=/etc/rsyslog.d
 LOGROTATE_DIR=/etc/logrotate.d
 LOG_FILE=$LOG_DIR/platform.conf
 LOGROTATE_FILE=$LOGROTATE_DIR/platform
+LOG_CONF_SRC_FILE=${INS_SRC_DIR}/app/rsyslog.d/platform.conf
+LOGROTATE_SRC_FILE=${INS_SRC_DIR}/app/rsyslog.d/platform
+CRON_SRC_FILE=${INS_SRC_DIR}/app/rsyslog.d/platform.cron
 install_platform()
 {
 	echo "install platform!"
@@ -14,13 +17,17 @@ install_platform()
 	install -m 0755 ${INS_SRC_DIR}/app/compile.info /etc/
 	install -m 0755 ${INS_SRC_DIR}/app/platform_version /etc/
 	
-	if [ ! -f "$LOG_FILE" ]; then
-		install -m 0644 ${INS_SRC_DIR}/app/rsyslog.d/platform.conf $LOG_FILE
+	if [ -f "$LOG_CONF_SRC_FILE" ]; then
+		install -m 0644 $LOG_CONF_SRC_FILE $LOG_FILE
 		service rsyslog restart
 	fi
-	if [ ! -f "$LOGROTATE_FILE" ]; then
-		install -m 0644 ${INS_SRC_DIR}/app/rsyslog.d/platform $LOGROTATE_FILE 
+	if [ -f "$LOGROTATE_SRC_FILE" ]; then
+		install -m 0644 $LOGROTATE_SRC_FILE $LOGROTATE_FILE 
 	fi
+	if [ -f "$CRON_SRC_FILE" ]; then
+		crontab $CRON_SRC_FILE
+	fi
+	
 	#echo "install router table!"
 	#install -m 0755 ${INS_SRC_DIR}/app/nr_config.json /etc/
 	#install -m 0755 ${INS_SRC_DIR}/app/sroute ${INS_PLATFOR_DIR}
